4796ea600875f8a7bd297ddc5db025402a007a0f
itmanesh <39644418+itmanesh@users.noreply.github.com>
2020-11-11 10:53:53 +0330

    Update new.java

--- src/main/webapp/new.java
+++ src/main/webapp/new.java
@@ -1,6 +1,3 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-	 @WithMockUser(username = "guest", password = "guest")
-    }  
-}  
+public class MyClass {
+  int x = 5;
+}
